e4778c7b8f69ac88830d1e7a3e7b0b96afd0421b
Emmanuel Ugaban <ugabane0516@gmail.com>
2022-11-27 23:46:05 +0800

    database

--- database.php
+++ database.php
@@ -8,6 +8,6 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 
 if(!$conn)
     {
-        die('Could not Connect My Sql:' .mysql_error());
+        die('Could not Connect My Sql:'.mysqli_error());
     }
 ?>
